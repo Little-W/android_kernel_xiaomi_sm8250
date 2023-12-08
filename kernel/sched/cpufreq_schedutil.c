90175b642e5e9e3a3dd93c7bd06296825b28d9e8
Yusen <1405481963@qq.com>
2023-12-08 21:57:01 +0800

    cpufreq: schedutil: set up rate-limit down rate-limit in configs
    Signed-off-by: Yusen <1405481963@qq.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1814,8 +1814,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	
 	if (cpumask_test_cpu(sg_policy->policy->cpu, cpu_lp_mask)) {
-		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 500;
+		tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_LP;
+		tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_LP;
 		tunables->powersave_freq = CONFIG_DEFAULT_POWERSAVE_FREQ_LP;
 		tunables->adaptive_up_freq = default_adaptive_up_freq_lp;
     	tunables->nadaptive_up_freq = ARRAY_SIZE(default_adaptive_up_freq_lp);
@@ -1833,8 +1833,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->limit_down = false;
 		tunables->limit_freq_userspace_ctl = true;
 	} else if (cpumask_test_cpu(sg_policy->policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 1000;
-		tunables->down_rate_limit_us = 500;
+		tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_PERF;
+		tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_PERF;
 
 		tunables->powersave_freq = CONFIG_DEFAULT_POWERSAVE_FREQ_HP;
 		tunables->adaptive_up_freq = default_adaptive_up_freq_hp;
@@ -1852,8 +1852,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->limit_down = false;
 		tunables->limit_freq_userspace_ctl = false;
 	} else {
-		tunables->up_rate_limit_us = 10000;
-		tunables->down_rate_limit_us = 1000;
+		tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_PRIME;
+		tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_PRIME;
 		
 		tunables->powersave_freq = CONFIG_DEFAULT_POWERSAVE_FREQ_PR;
 		tunables->adaptive_up_freq = default_adaptive_up_freq_pr;
