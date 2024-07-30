NextWork123/kernel_xiaomi_sm8250

fs: exec: Swich to task_is_zygote

kernel/cgroup/cgroup-v1.c
@@ -541,11 +541,12 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
 
 	/* This covers boosting for app launches and app transitions */
-	if (!ret && !threadgroup &&
-		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
-		is_zygote_pid(task->parent->pid)) {
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 500);
-	}
+        if (!ret && !threadgroup &&
+               !memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
+               task_is_zygote(tsk->parent)) {
+                devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 1000);
+
+        }
 
 out_finish:
 	cgroup_procs_write_finish(task);

kernel/fork.c
@@ -2382,9 +2382,8 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 150 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid)) {
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 150);
+	if (task_is_zygote(current)) {
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 50);
 	}
 
 	/*

Little-W/android_kernel_xiaomi_sm8250

Signed-off-by: Little-W <1405481963@qq.com>

kernel/cgroup/cgroup-v1.c
@@ -543,6 +543,14 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
 
+	/* This covers boosting for app launches and app transitions */
+        if (!ret && !threadgroup &&
+               !memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
+               task_is_zygote(task->parent)) {
+                devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW_BOOST_FREQ, 1000);
+
+        }
+
 out_finish:
 	cgroup_procs_write_finish(task);
 out_unlock:

kernel/fork.c
@@ -2377,6 +2377,10 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
+	if (task_is_zygote(current)) {
+		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW_BOOST_FREQ, 50);
+	}
+
 	/*
 	 * Determine whether and which event to report to ptracer.  When
 	 * called from kernel_thread or CLONE_UNTRACED is explicitly